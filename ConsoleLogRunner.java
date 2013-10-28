79c739b68fb4b5131fb5390f29abd3cf49ad1107
joysheaven <joysheaven@gmail.com>
2013-10-29 00:23:51 +0530

    ConsoleLogRunner.java

---
+++ ConsoleLogRunner.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ConsoleLogRunner 
+{
+	public static void main(String[] args)
+	{
+		ConsoleLog c = new ConsoleLog();
+		c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		c.setSize(100,100);
+		c.setVisible(true);
+	}
+}
